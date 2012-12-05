2f2d64ef6af362ade48729d0cd3eeaaec3d6fe2c
Jim Ruther Nill <jim@netengine.com.au>
2012-12-05 16:00:35 +0800

    add test for install generator

---
+++ spec/install_generator_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+require 'rails/generators'
+require 'generators/track_me/install/install_generator'
+require 'generator_spec/test_case'
+
+describe TrackMe::InstallGenerator do
+  include GeneratorSpec::TestCase
+  destination File.expand_path("../../tmp", __FILE__)
+
+  before do
+    prepare_destination
+    run_generator
+  end
+
+  specify 'config file is properly generated' do
+    assert_file 'config/initializers/track_me.rb', /ENABLED_METRICS/
+  end
+
+  specify 'metric templates are properly generated' do
+    assert_file 'app/views/track_me/_js.html.haml'
+    assert_file 'app/views/track_me/_mixpanel.html.haml', /Initialize Mixpanel/
+    assert_file 'app/views/track_me/_kissmetrics.html.haml', /Initialize Kissmetrics/
+  end
+end
