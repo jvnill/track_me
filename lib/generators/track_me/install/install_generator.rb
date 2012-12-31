6998a86848899b43ba89602d9ea0a6eabcba4dbf
Jim Ruther Nill <jim@netengine.com.au>
2012-12-31 13:37:38 +0800

    separate init files and actual js that sends data to support pjax and turbolinks

--- lib/generators/track_me/install/install_generator.rb
+++ lib/generators/track_me/install/install_generator.rb
@@ -4,16 +4,23 @@ module TrackMe
 
     def copy_files
       copy_file '_js.html.haml', 'app/views/track_me/_js.html.haml'
+      copy_file '_init.html.haml', 'app/views/track_me/_init.html.haml'
       copy_file '_mixpanel.html.haml', 'app/views/track_me/_mixpanel.html.haml'
+      copy_file '_mixpanel_init.html.haml', 'app/views/track_me/_mixpanel_init.html.haml'
       copy_file '_kissmetrics.html.haml', 'app/views/track_me/_kissmetrics.html.haml'
+      copy_file '_kissmetrics_init.html.haml', 'app/views/track_me/_kissmetrics_init.html.haml'
       copy_file 'track_me.rb', 'config/initializers/track_me.rb'
 
       say "\n"
-      say 'Please dont forget to add the js file to the head section of your html templates', :green
+      say 'Please dont forget to add the init file to the head section of your html templates.  The main reason why the init file is separated is for applications that use pjax or turbolinks where the head section is only loaded once.', :green
       say "\n"
-      say "  = render 'track_me/js' if Rails.env.production?", :green
+      say "  = render 'track_me/init'", :green
       say "\n"
-      say 'and the call to include on application controller', :green
+      say "You can then add the partial that actually sends the data"
+      say "\n"
+      say "  = render 'track_me/js'", :green
+      say "\n"
+      say 'and then include the main module on application controller', :green
       say "\n"
       say "  include TrackMe", :green
       say "\n"
